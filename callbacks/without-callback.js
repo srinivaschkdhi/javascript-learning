d993081abfb4b7a82995aa72eb95f1f626c6fd09
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2021-12-29 06:47:03 +0530

    added callbacks

---
+++ callbacks/without-callback.js
@@ -0,0 +1,12 @@
+const geocode = (address,callback)=>{
+    setTimeout(() => {
+        const dataa = {
+            latitude:0,
+            longitude:0
+        }
+        return dataa;
+    }, 2000);
+}
+
+const data = geocode('Philadelphia')
+console.log(data)
